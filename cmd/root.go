c8d7b49fff3da131281f2b16c58d644a0bce8dad
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-12-30 21:53:14 +0700

    add an option to input certificate as an arg

--- cmd/root.go
+++ cmd/root.go
@@ -12,18 +12,29 @@ import (
 )
 
 var (
-	file    string
-	rootCmd = &cobra.Command{
+	file               string
+	encodedCertificate string
+	rootCmd            = &cobra.Command{
 		Use:   "fgo-certificate-extractor",
 		Short: "Extract the certificate from the FGO data file",
 		Long:  `Extract the certificate from the FGO data file`,
 		Run: func(cmd *cobra.Command, args []string) {
-			certificateByte, err := os.ReadFile(file)
-			if err != nil {
-				panic(err)
+			if file == "" && len(args) == 0 {
+				fmt.Println("Please specify the certificate file or the certificate string")
+				os.Exit(1)
+			}
+			if file != "" {
+				certificateByte, err := os.ReadFile(file)
+				if err != nil {
+					panic(err)
+				}
+
+				encodedCertificate = string(certificateByte[2:])
+			}
+			if len(args) != 0 {
+				encodedCertificate = args[0]
 			}
 
-			encodedCertificate := string(certificateByte[2:])
 			certificate, err := extract.Extract(encodedCertificate)
 			if err != nil {
 				panic(err)
@@ -47,5 +58,4 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().StringVarP(&file, "file", "f", "", "Certificate file")
-	rootCmd.MarkFlagRequired("file")
 }
